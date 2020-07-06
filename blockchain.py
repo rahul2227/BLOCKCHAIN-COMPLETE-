0507d34bdb867d0df3975966e604d3480b22fd9d
rahul2227 <rahul2227.l115@gmail.com>
2020-07-06 22:05:42 +0530

    blockchain first file

---
+++ blockchain.py
@@ -0,0 +1,38 @@
+# Creating a blockchain
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#blockchain
+
+class BlockChain:
+    
+    def __init__(self):
+        self.chain = []
+        self.Create_Block(proof = 1, previous_hash = '0')
+        
+    def Create_Block(self, proof, previous_hash):
+        block = {'index' : len(self.chain) + 1,
+                 'timestamp' : str(datetime.datetime.now()),
+                 'proof' : proof, 
+                 'previous_hash' : previous_hash}
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1] # for the last block of the blockchain
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    
